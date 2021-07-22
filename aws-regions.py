2218bb9b36f8c17f562d52834b0280d42e7653a7
groorj <gustavo.aguiar@gmail.com>
2021-07-22 08:33:32 -0400

    #1 implemented an access validation using HTTP headers

--- aws-regions.py
+++ aws-regions.py
@@ -11,6 +11,24 @@ logger = logging.getLogger()
 logger_level = logging.getLevelName(os.environ['LOGGER_LEVEL'])
 logger.setLevel(logger_level)
 
+# validate access
+def validate_access(event, context):
+    logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
+    logger.debug("RESTRICTED_ACCESS_ENABLED: [%s]", os.environ['RESTRICTED_ACCESS_ENABLED'])
+    error_message = "You are not allowed, get out!"
+    if os.environ['RESTRICTED_ACCESS_ENABLED'] == 'true':
+        logger.info("Restricted access is enabled")
+        logger.info("Value for header [%s] is: [%s]", os.environ['RESTRICTED_ACCESS_HTTP_HEADER'], event["headers"][os.environ['RESTRICTED_ACCESS_HTTP_HEADER']])
+        if event["headers"][os.environ['RESTRICTED_ACCESS_HTTP_HEADER']] != os.environ['RESTRICTED_ACCESS_SECRET']:
+            logger.info("Key provided is not valid")
+            logger.debug("Error: [%s]", error_message)
+            http_code = 403
+            raise ValueError(http_code, error_message)
+        else:
+            logger.info("Key provided is valid")
+    else:
+        logger.info("Restricted access is NOT enabled")
+
 # create response
 def create_response_new(status_code, message_body):
     logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
@@ -35,10 +53,17 @@ def get_json():
     json_data = json.loads(response.read())
     return json_data
 
-# return region info
+# entry point -> return region info
 def get_region_info(event, context):
     logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
     return_info_final = {}
+    # validate the access to this resource
+    try:
+        validate_access(event, context)
+    except ValueError as err:
+        return_info_final['request'] = { "request_status": "Fail", "error_message": err.args[1], "http_error_code": err.args[0] }
+        return create_response_new(err.args[0], return_info_final)
+    # get region info
     region_code = event['pathParameters']['region_code']
     logger.debug("region_code: [%s]", region_code)
     try:
@@ -48,7 +73,7 @@ def get_region_info(event, context):
         http_code = 500
         return_info_final['request'] = { "request_status": "Fail", "error_message": "Error getting Regions information.", "http_error_code": err.code }
         return create_response_new(http_code, return_info_final)
-    logger.debug("json_data: [%s]", json_data)
+    # logger.debug("json_data: [%s]", json_data)
     # logger.debug("type(json_data): [%s]", type(json_data))
     for element in json_data['data']:
         # logger.debug("code: [%s] && region_code: [%s]", element['code'], region_code)
@@ -66,10 +91,17 @@ def get_region_info(event, context):
             return_info_final['request'] = { "request_status": "Fail", "error_message": "Region code NOT found.", "http_error_code": http_code }
     return create_response_new(http_code, return_info_final)
 
-# return region info
+# entry point -> return region info
 def get_all_regions_info(event, context):
     logger.debug("Inside function: [%s]", inspect.currentframe().f_code.co_name)
     return_info_final = {}
+    # validate the access to this resource
+    try:
+        validate_access(event, context)
+    except ValueError as err:
+        return_info_final['request'] = { "request_status": "Fail", "error_message": err.args[1], "http_error_code": err.args[0] }
+        return create_response_new(err.args[0], return_info_final)
+    # get regions info
     try:
         json_data = get_json()
     except HTTPError as err:
